8e3b0222a421748c3ef224281122e4c45a5bf12b
hmngwy <hmngwy@users.noreply.github.com>
2018-07-19 23:49:01 +0800

    Inverted colors

--- .layout/index.sh
+++ .layout/index.sh
@@ -43,9 +43,13 @@ cat << _EOF_
     <link href="https://fonts.googleapis.com/css?family=Overpass+Mono:400,700" rel="stylesheet">
     <link href="data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAANjY2AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAARERERAAAAAAAAAAAAAAAAEQEBEQAAAAAAAAAAAAAAABEREREAAAAAAAAAAAAAAAARAREBAAAAAAAAAAAAAAAAEREBEQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//wAA//8AAP//AADwDwAA//8AAPKPAAD//wAA8A8AAP//AADyLwAA//8AAPCPAAD//wAA//8AAP//AAD//wAA" rel="icon" type="image/x-icon">
     <style>
+      * {
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+      }
       body {
-        background-color: white;
-        color: #222;
+        background-color: rgb(27, 27, 27);
+        color: #434343;
         font-size: 16px;
         padding: 1em;
         font-family: 'Overpass Mono', monospace;
@@ -57,8 +61,8 @@ cat << _EOF_
       .post-link { display: table; text-transform: uppercase; margin-bottom: .55em; }
       .post-link a { text-decoration: none; line-height:2.5ch; display:inline-block; }
       .post-link .title:hover {  text-decoration: underline;  }
-      .post-link .stamp { color: #999; display: table-cell; width: 5ch; text-align:right; padding-right: 2ch; }
-      .post-link .title { color: #333; display: table-cell; vertical-align: top;font-weight: bold; }
+      .post-link .stamp { color: #676767; display: table-cell; width: 5ch; text-align:right; padding-right: 2ch; }
+      .post-link .title { color: #c9c9c9; display: table-cell; vertical-align: top;font-weight: bold; }
       nav a { color: #555; text-decoration: none; } nav a:hover { color: #268bd2}
       nav a+a:before { content: ' • '; }
       header { text-transform: uppercase; }
--- .layout/post.sh
+++ .layout/post.sh
@@ -8,12 +8,16 @@ cat << _EOF_
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       <title>$(echo $POST_TITLE) - Manila Functional</title>
       <link href="https://fonts.googleapis.com/css?family=Overpass+Mono:400,700" rel="stylesheet">
-      <link href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/arduino-light.min.css" rel="stylesheet">
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/obsidian.min.css" rel="stylesheet">
       <link href="data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAANjY2AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAARERERAAAAAAAAAAAAAAAAEQEBEQAAAAAAAAAAAAAAABEREREAAAAAAAAAAAAAAAARAREBAAAAAAAAAAAAAAAAEREBEQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//wAA//8AAP//AADwDwAA//8AAPKPAAD//wAA8A8AAP//AADyLwAA//8AAPCPAAD//wAA//8AAP//AAD//wAA" rel="icon" type="image/x-icon">
       <style>
+        * {
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+        }
         body {
-          background-color: white;
-          color: #222;
+          background-color: rgb(27, 27, 27);
+          color: #c1c1c1;
           font-size: 16px;
           padding: 1em;
           font-family: 'Overpass Mono', sans-serif;
@@ -23,21 +27,21 @@ cat << _EOF_
         article { padding: 0; margin: 1.2em 0; max-width: 87ch; }
         article a { color: #b58900; display: inline-block; }
         article img { max-width: 100% }
-        article blockquote { border-left: 2px solid #CCC; padding: 2ch; border-radius: 3px; background-color: #f4f4f4; }
+        article blockquote { border-left: 1ch solid #313131; padding: 1.5ch 2ch; border-radius: 3px; background-color: #202020; }
         article blockquote,
-        article pre { margin: 0; border-bottom: 1px solid #DDD; }
+        article pre { margin: 0; }
         article pre,
-        article code { background-color: #f4f4f4; border-radius: 3px; font-family: 'Overpass Mono'; color: #333; }
+        article code { background-color: #2b2b2b; border-radius: 3px; font-family: 'Overpass Mono'; }
         article pre { word-break: break-all; width: 100%; }
         article blockquote :first-of-type { margin-top: 0; }
         article blockquote :last-of-type { margin-bottom: 0; }
-        article hr { border: 0; border-bottom: 3px solid #CCC; }
+        article hr { border: 0; border-bottom: 3px solid #2d2d2d; }
         .heading { font-family: 'Overpass Mono'; margin-bottom: 2ch; }
         .heading a { text-decoration: none; }
-        .heading .title { max-width: 650px; font-size: inherit; color: #222; display: table-cell; text-transform: uppercase; margin: 0 0 1em; font-weight:bold; line-height: 2.5ch;  }
+        .heading .title { max-width: 650px; font-size: inherit; color: #c9c9c9; display: table-cell; text-transform: uppercase; margin: 0 0 1em; font-weight:bold; line-height: 2.5ch;  }
         .heading .title:hover { text-decoration: underline; }
         .home { display: inline-block; width: 5ch; text-align:right; margin-right: 2ch; }
-        .heading .stamp { color: #999; display: table-cell; width: 5ch; text-align:right; padding-right: 2ch; }
+        .heading .stamp { color: #676767; display: table-cell; width: 5ch; text-align:right; padding-right: 2ch; }
         .home { text-decoration: none; margin-bottom: 1.5em; text-align: left;  color: #cb4b16; } .home:hover { color: #dc322f; }
         .contents > * { margin: 2ch 0; }
         .contents { display: inline-block; max-width: 80ch; vertical-align: top; width: 100%; }
@@ -59,13 +63,13 @@ cat << _EOF_
         .footnotes { padding: 1em 0 0; font-size: .85em; }
         .footnotes:before { content: 'FOOTNOTES' }
         .footnotes hr { display: none; }
-        .footnotes ol { padding-left: 2.85em; }
+        .footnotes ol { padding-left: 2.85em; line-height: 3ch; }
         .footnote { vertical-align: super; font-size: .85em; text-decoration: none; line-height: 0; }
         .tags { margin-top: 3em; font-size: .85em; }
-        .tags a { background-color: #EEE; display:inline-block;padding: 0 .5em;border-radius: 4px; }
+        .tags a { background-color: #202020; display:inline-block;padding: 0 .5em;border-radius: 4px; }
         .wrap { max-width: 1024px; margin: 0 auto; }
         figure { margin: 0 }
-        .hljs { background-color: #fbfbfb; }
+        .hljs { background-color: #202020; }
         footer { margin-top: 5ch; } footer a { color: inherit; }
         footer span { font-size:.85em; color: grey; }
       </style>
